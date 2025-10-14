6ca278ef53cfde9bb332067dcf3c4e04a8c6cc8b
Sergei Finogenov <serfinogenov@yandex.ru>
2025-10-14 16:00:59 +0300

    creating model "User"; applying migrations

--- users/models.py
+++ users/models.py
@@ -1,5 +1,35 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email",
+    )
+
+    phone_number = models.CharField(
+        max_length=25,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+    )
+
+    city = models.CharField(
+        max_length=100,
+        verbose_name="Город",
+        blank=True,
+        null=True,
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
