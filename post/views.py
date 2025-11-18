6eea886a2b4da463662ae23a5ed00d446838449e
Sergei Finogenov <serfinogenov@yandex.ru>
2025-11-18 23:47:55 +0300

    fixing the config urls; fixing the field 'commentary' of the model Post; applying migrations

--- post/views.py
+++ post/views.py
@@ -125,9 +125,3 @@ class PostViewSet(ModelViewSet):
 #
 #         return Response({"message": message})
 #
-#
-# # class CurrentUserAPIView(APIView):
-# #     def get_serializer_context(self):
-# #         context = super().get_serializer_context()
-# #         context['request'] = self.request
-# #         return context
