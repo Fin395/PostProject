a311e12dad3f3ee7020fd844533077d2755ea5c1
Sergei Finogenov <serfinogenov@yandex.ru>
2025-11-28 15:06:52 +0300

    adding the admin for the Commentary model

--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from post.models import Post
+from post.models import Post, Commentary
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'author')
-    exclude = ['password']
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'author')
