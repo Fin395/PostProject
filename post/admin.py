630e50f55c13c35f79d0466fa970fc88049f59d8
Sergei Finogenov <serfinogenov@yandex.ru>
2025-11-28 15:37:36 +0300

    updating the admin for the Post model to filter the posts by the date of creation

--- post/admin.py
+++ post/admin.py
@@ -6,7 +6,7 @@ from post.models import Post, Commentary
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'content', 'author')
-
+    list_filter = ('created_at',)
 
 @admin.register(Commentary)
 class CommentaryAdmin(admin.ModelAdmin):
